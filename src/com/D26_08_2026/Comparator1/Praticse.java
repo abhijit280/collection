153c8143dee047dc9745ca9528801f0e631fd516
abhijit280 <abhijittapu65615@gmail.com>
2026-08-28 19:16:48 +0530

    ..

--- src/com/D26_08_2026/Comparator1/Praticse.java
+++ src/com/D26_08_2026/Comparator1/Praticse.java
@@ -13,19 +13,21 @@ public class Praticse {
 		List<Products> list = new ArrayList<Products>();
 		list.add(new Products(123, "shoes", 2000));
 		list.add(new Products(145, "book", 200));
-		list.add(new Products(123, "Bag", 4000));
-		list.add(new Products(123, "IPhone", 200000));
-		list.add(new Products(123, "Laptop", 80000));
+		list.add(new Products(178, "Bag", 4000));
+		list.add(new Products(189, "IPhone", 200000));
+		list.add(new Products(156, "Laptop", 80000));
 		 
-		Comparator<Products>comparator = (x,y)->Integer.compare(x.prdId, y.prdId);
-		Collections.sort(list,comparator);
+		Comparator<Products>compareByPrice = (x,y)->Integer.compare(x.prdPrice, y.prdPrice);
+		Comparator<Products>compareById = (x,y)->Integer.compare(x.prdId, y.prdId);
+		Comparator<Products>compareByPrdName = (x,y)->x.prdName.compareTo(y.prdName);
+		Collections.sort(list,compareByPrdName);
 		Iterator<Products> itr = list.iterator();
 		while (itr.hasNext()) {
 			Products prd = itr.next();
 			System.out.println(prd.prdId +" , "+prd.prdName+" , "+prd.prdPrice);
 			
 		}
-		System.out.println(list);
+	//	System.out.println(list);
 
 	}
 
